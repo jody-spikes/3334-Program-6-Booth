bdc728900c91e13fb12880ef5dc3c11c9c4dcd10
jody-spikes <jodyspikes2003@icloud.com>
2026-04-08 17:44:27 -0500

    Remove function and style stuff

---
+++ Program 6 (git)/Program 6 (git)/HashTable.cpp
@@ -0,0 +1,190 @@
+//
+//  HashTable.cpp
+//  Program 6
+//
+//  Created by Jody Spikes on 3/17/26.
+//
+
+#include <stdio.h>
+#include "HashTable.h"
+
+//------
+//Insert
+//------
+template <typename Key, typename Value>
+bool HashTable<Key, Value>::insert(const Key& key, const Value& value) {
+    if(loadFactor() >= maxLoadFactor_){
+        rehash();
+    }
+    
+    size_t idx = bucketIndex(key);
+    
+    size_t pos = findEntryPosition(idx, key);
+    
+    if(pos != buckets_[idx].size()){
+        buckets_[idx][pos].value = value;
+        return false; //no key update
+    }
+    
+    if(!buckets_[idx].empty()){
+        ++collisionCount_;
+    }
+    
+    buckets_[idx].emplace_back(key, value);
+    ++elementCount_;
+    return true;
+    
+}
+
+//--------
+//Contains
+//--------
+    template <typename Key, typename Value>
+    bool HashTable<Key, Value>::contains(const Key& key) const {
+        size_t idx = bucketIndex(key);
+        return findEntryPosition(idx, key) != buckets_[idx].size();
+    }
+
+//------
+//Remove
+//------
+template <typename Key, typename Value>
+bool HashTable<Key, Value>::remove(const Key& key) {
+    size_t idx = bucketIndex(key);
+    size_t pos = findEntryPosition(idx, key);
+    
+    if(pos == buckets_[idx].size()){
+        return false;
+    }
+    
+    buckets_[idx][pos] = buckets_[idx].back();
+    buckets_[idx].pop_back();
+    --elementCount_;
+    return true;
+}
+
+//----------------
+//Find (not const)
+//----------------
+template <typename Key, typename Value>
+Value* HashTable<Key, Value>::find(const Key& key) {
+    size_t idx = bucketIndex(key);
+    size_t pos = findEntryPosition(idx, key);
+    if(pos == buckets_[idx].size()) {
+        return nullptr;
+    }
+    return &buckets_[idx][pos].value;
+}
+
+//----------------
+//Find (const)
+//----------------
+template <typename Key, typename Value>
+const Value* HashTable<Key, Value>::find(const Key& key) const {
+    size_t idx = bucketIndex(key);
+    size_t pos = findEntryPosition(idx, key);
+    if(pos == buckets_[idx].size()) {
+        return nullptr;
+    }
+    return &buckets_[idx][pos].value;
+}
+
+//-------------
+//at (not const)
+//-------------
+template <typename Key, typename Value>
+Value& HashTable<Key, Value>::at(const Key& key) {
+    Value* val = find(key);
+    if (!val) {
+        throw out_of_range("Key not found");
+    }
+    return *val;
+}
+
+//-------------
+//at (const)
+//-------------
+template <typename Key, typename Value>
+const Value& HashTable<Key, Value>::at(const Key& key) const {
+    const Value* val = find(key);
+    if (!val) {
+        throw out_of_range("Key not found");
+    }
+    return *val;
+}
+//
+//    void rehash() {}
+//
+//    size_t size() const {}
+//
+//    size_t tableSize() const {}
+//
+//    double loadFactor() const {}
+//
+//    size_t collisionCount() const {}
+//
+//    size_t rehashCount() const {}
+//
+//    vector<Key> keys() const {}
+//
+//private:
+//    vector<vector<Entry> > buckets_;
+//    size_t elementCount_;
+//    size_t collisionCount_;
+//    size_t rehashCount_;
+//    double maxLoadFactor_;
+
+//------------
+//Bucket Index
+//------------
+template <typename Key, typename Value>
+size_t HashTable<Key, Value>::bucketIndex(const Key& key) const {
+    return hash<Key>{}(key) % buckets_.size();
+}
+
+//-------------------
+//Find Entry Position
+//-------------------
+template <typename Key, typename Value>
+size_t HashTable<Key, Value>::findEntryPosition
+(size_t bucket, const Key& key) const {
+    const auto& chain = buckets_[bucket];
+    for(size_t i = 0; i < chain.size(); i++){
+        if(chain[i].key == key){
+            return i;
+        }
+    }
+    return chain.size();
+}
+//
+//    void insertWithoutStats(const Key& key, const Value& value) {}
+//
+
+//--------
+//Is Prime
+//--------
+template <typename Key, typename Value>
+bool HashTable<Key, Value>::isPrime(size_t n) {
+      if(n < 2)      return false;
+      if(n == 2)     return true;
+      if(n % 2 == 0) return false;
+
+      for(size_t i = 3; i * i <= n; i += 2){
+          if(n % i == 0) return false;
+      }
+
+      return true;
+  }
+
+//----------
+//Next Prime
+//----------
+template <typename Key, typename Value>
+size_t HashTable<Key, Value>::nextPrime(size_t n) {
+    if(n < 2) return 2;
+
+    while(!isPrime(n)){
+        ++n;
+    }
+    return n;
+}
